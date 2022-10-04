0653b361e1c951533e5fe2430d2177eb52d9ce1c
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-04 17:27:28 +0200

    Add project files.

---
+++ Kappitel 3 uppgfit 3.20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt första ord");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv in ditt andra ord");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Skriv in diit tredje ord");
+            string ord3 = Console.ReadLine();
+
+            string[] ordning = { ord1, ord2, ord3 };
+            var ordningrätt = ordning.OrderBy(n => n);
+            foreach (var namn in ordningrätt)
+            {
+                Console.WriteLine(namn);
+            }
+        }
+    }
+}
